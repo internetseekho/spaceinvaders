6f4b75abbd14693b49f87c633ee7c4c94f1fdd7a
root <mjks4545@gmail.com>
2018-06-16 09:47:33 -0700

    Code update till 9 video
    We have complete the space invaders project till player laser shooting...

---
+++ Assets/Entity/Enemy/EnemySpawer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawer : MonoBehaviour {
+
+    public GameObject enemyPrefab;
+    public float width = 10f;
+    public float height = 5f;
+
+    public float speed = 5f;
+    bool movingLeft = true;
+    float xmin;
+    float xmax;
+    // Use this for initialization
+    void Start () {
+
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftMost = Camera.main.ViewportToWorldPoint( new Vector3(0, 0, distance) );
+        Vector3 rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+
+        xmin = leftMost.x;
+        xmax = rightMost.x;
+
+        foreach (Transform child in transform) {
+            GameObject Enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
+            Enemy.transform.parent = child;
+        }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3( width, height, 0 ) );
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if ( movingLeft ) {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        } else {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        float rightEdgeOfspawner = transform.position.x + (0.5f * width);
+        float leftEdgeOfspawner = transform.position.x - (0.5f * width);
+
+        if (leftEdgeOfspawner < xmin) {
+            movingLeft = false;
+        } else if (rightEdgeOfspawner > xmax) {
+            movingLeft = true;
+        }
+
+    }
+
+}
---
+++ Assets/Entity/Player/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 5.0f;
+    public GameObject projectile;
+    public float procjecttileSpeed = 5f;
+    public float firingRate = 0.2f;
+
+    float xmin;
+    float xmax;
+    float padding = 0.5f;
+    // Use this for initialization
+    void Start () {
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        // is bit 0 1 0 is the left most point and 1 is the right most. 0.5
+        Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3( 0, 0,distance));
+        Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+        xmin = leftmost.x + padding;
+        xmax = rightmost.x - padding;
+    }
+
+    void fire() {
+        GameObject beam = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+        beam.GetComponent<Rigidbody2D>().velocity = new Vector3(0f, procjecttileSpeed, 0f);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            InvokeRepeating("fire", 0.000001f, firingRate);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space)) {
+            CancelInvoke("fire");
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        } else if (Input.GetKey(KeyCode.RightArrow)) {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+        transform.position = new Vector3( newX, transform.position.y, transform.position.z );
+	}
+}
